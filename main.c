9db602c14f5dedc7df5d8f66e622beb33d935ac1
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-10-10 15:51:43 +0900

    2

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+int sumtwo(int a, int b)
+{
+
+	//int output;
+	//output = a+b;
+	return a+b;//output;
+}
+
+int square(int n)
+{
+	return n*n;
+}
+int get_max(int a, int b)
+{
+	if (a>b)
+	  return a;
+	  
+	return b; 
+}
+	return 0;
+}
